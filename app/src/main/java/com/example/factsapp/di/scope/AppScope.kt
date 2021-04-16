80dd95fd2d9de8cb96b357fad01b32199486a88e
avez raj <avais.983@gmail.com>
2021-04-17 00:02:52 +0530

    refractor work done

--- app/src/main/java/com/example/factsapp/di/scope/AppScope.kt
+++
@@ -1,7 +0,0 @@
-package com.example.factsapp.di.scope
-
-import javax.inject.Scope
-
-@Scope
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class AppScoped
--- app/src/main/java/com/example/factsapp/repository/RepositoryInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.example.factsapp.repository
-
-import com.example.factsapp.model.Fact
-import com.example.factsapp.network.ResultWrapper
-
-interface RepositoryInterface {
-   suspend fun getFactsFeeds(): ResultWrapper<Fact>
-}
